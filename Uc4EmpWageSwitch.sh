44ace7c32ea3c7bfdd7ff9dbfa7c1006a42bca10
Ramade123 <rameshwarade340@gmail.com>
2022-08-30 18:47:56 +0530

    [Rameshwar] add Uc4EmpWageSwitch

---
+++ Uc4EmpWageSwitch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
+
+case $randomCheck in
+                      $isPartTime)
+                             empHr=4
+                      ;;
+                      $isFullTime)
+                             empHr=8
+                      ;;
+                       *)
+                             empHr=0
+                      ;;
+esac
+salary=$(($empHr*$empRatePerHr))
